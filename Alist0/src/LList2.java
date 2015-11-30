dc73ac942a785000cb33b080ef9186ce4a66fbad
denisova <denisova@Alexander-Zarembas-Mac-mini.local>
2015-11-30 17:19:15 +0200

    qqq

--- Alist0/src/LList2.java
+++ Alist0/src/LList2.java
@@ -44,80 +44,119 @@ public class LList2 implements EList {
 
 	@Override
 	public int[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		int[] ret = new int[size()];
+		Node tmp = front;
+		int i = 0;
+		while (tmp != null) {
+			ret[i++] = tmp.val;
+			tmp = tmp.next;
+		}
+		return ret;
 	}
 
 	@Override
 	public void set(int pos, int val) {
+		if (size() == 0)
+			throw new IllegalArgumentException();
+		if (size() <= pos)
+			throw new ArrayIndexOutOfBoundsException();
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public int get(int pos) {
+		if (front == null)
+			throw new IllegalArgumentException();
+		if (size() < pos)
+			throw new ArrayIndexOutOfBoundsException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void addStart(int val) {
-		Node p = new Node(val);
-		p.next = front;
-		front = p; 
+		Node tmp = new Node(val);
+		tmp.next = front;
+		front = tmp;
 //		p.next.prev = front;
 	}
 
 	@Override
 	public void addEnd(int val) {
+		if (front == null) {
+			front = new Node(val);
+			return;
+		}
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void addPos(int pos, int val) {
+		if (size() == 0 || size() < pos)
+			throw new ArrayIndexOutOfBoundsException();
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public int delStart() {
+		if (front == null) {
+			throw new NegativeArraySizeException();
+		}
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int delEnd() {
+		if (size() <= 0)
+			throw new NegativeArraySizeException();
+		if (size() == 1)
+			throw new ArrayIndexOutOfBoundsException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int delPos(int pos) {
+		if (size() <= 0)
+			throw new NegativeArraySizeException();
+		if (size() <= pos)
+			throw new ArrayIndexOutOfBoundsException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int min() {
+		if (size() == 0)
+			throw new IllegalArgumentException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int max() {
+		if (size() == 0)
+			throw new IllegalArgumentException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int minIndex() {
+		if (size() == 0)
+			throw new IllegalArgumentException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int maxIndex() {
+		if (size() == 0)
+			throw new IllegalArgumentException();
 		// TODO Auto-generated method stub
 		return 0;
 	}
